b72c7e42629c1617493f3c3c0408e34972ab8cd5
dongl <dongl@sungrowMacBook-Pro.local>
2017-08-30 18:52:51 +0800

    Initial Commit

---
+++ ChartsDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChartsDemo
+//
+//  Created by sungrow on 2017/8/30.
+//  Copyright © 2017年 yuqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
